dc3603a26ba1088c40354a986115094cd24cbe5c
vincloud2 <128999303+vincloud2@users.noreply.github.com>
2023-06-28 21:33:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIA454X247ECJR6TTEI"
-  secret_key = "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"
+  access_key = "AKIAZWCN7RXXVHRJVU4J"
+  secret_key = "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"
   profile    = "default"
   region     = "ap-southeast-1"
 }
